1ddea95170d11a8cc06d36744d3c346da05aaa26
figeral <mouliofitzgerard@gmail.com>
2023-10-02 10:53:32 +0100

    first java app

---
+++ src/main/java/com/figeral/CompoundInterest04.java
@@ -0,0 +1,35 @@
+package compoundinterest.src.main.java.com.figeral;
+
+import java.text.NumberFormat;
+public class CompoundInterest04 {
+     private final double principal = 100.0;
+    private final double annualInterestRate = 0.05;
+    private final double compoundPerTimeUnit = 12.0;
+    private final double time = 5.0; //
+    private final NumberFormat currencyFormat;
+    private final NumberFormat percentFormat;
+    public CompoundInterest04() {
+        currencyFormat =
+        NumberFormat.getCurrencyInstance();
+        percentFormat = NumberFormat.getPercentInstance();
+        percentFormat.setMinimumFractionDigits(0);
+        percentFormat.setMaximumFractionDigits(5);
+}
+  public void perform() {
+        var result = calculateCompoundInterest();
+            System.out.printf("If you deposit %s in a savingsaccount " + "that pays %s annual interest compounded monthly%n" + "you will have after %1.0f years %s",
+            currencyFormat.format(principal),
+            percentFormat.format(annualInterestRate),
+             time, currencyFormat.format(result));
+      }
+      private double calculateCompoundInterest() {
+        var result = principal * Math.pow(1 +
+            annualInterestRate / compoundPerTimeUnit,
+            time * compoundPerTimeUnit);
+            return result;
+       }
+        public static void main(String[] args) {
+           var banker = new CompoundInterest04();
+           banker.perform();
+        }
+}
